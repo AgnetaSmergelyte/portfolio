d9d5c742d43eebe735683c25918676e89351e372
spegisima <agneta.smergelyte@gmail.com>
2023-10-26 09:48:16 +0300

    pretier errors

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,13 +1,21 @@
 import React, {useState} from 'react';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faFacebookSquare, faLinkedin} from "@fortawesome/free-brands-svg-icons";
+import {faCheck, faExclamationTriangle} from '@fortawesome/free-solid-svg-icons';
 
 const Contact = () => {
     const [errorMsg, setErrorMsg] = useState('');
-    const [sent, setSent] = useState(false);
+    const [successMsg, setSuccessMsg] = useState('');
+    const [sent, setSent] = useState(false)
 
     function sendMessage() {
-        setErrorMsg('Message not sent')
+        if (sent) {
+            setSuccessMsg(' Message sent');
+            setErrorMsg('')
+        } else {
+            setErrorMsg(' Message not sent');
+            setSuccessMsg('');
+        }
         setSent(!sent);
     }
 
@@ -17,8 +25,8 @@ const Contact = () => {
                 <h1 className="text-center">Feel free to drop me a message <em>or let's be social!</em></h1>
                 <input type="text" placeholder="Your email address"/>
                 <textarea placeholder="Message" rows="5"/>
-                {errorMsg &&
-                    <div className="text-center"><b className={sent ? "text-green" : "text-red"}>{errorMsg}</b></div>}
+                {errorMsg && <b className="text-center text-red"><FontAwesomeIcon icon={faExclamationTriangle} /> {errorMsg}</b>}
+                {successMsg && <b className="text-center text-green"><FontAwesomeIcon icon={faCheck} /> {successMsg}</b>}
                 <button className="btn-main" onClick={sendMessage}>Send</button>
                 <div className="d-flex gap-1 a-center social mt-1">
                     <h2>Socials:</h2>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import profilePic from '../images/profile-pic.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";

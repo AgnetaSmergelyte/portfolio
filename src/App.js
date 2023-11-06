1f02a14cc9e2ad3cc8edb3df9df3326fa48c5968
spegisima <agneta.smergelyte@gmail.com>
2023-11-06 09:26:06 +0200

    wakeup fun

--- src/App.js
+++ src/App.js
@@ -2,26 +2,32 @@ import './App.css';
 import Home from "./pages/Home";
 import Portfolio from "./pages/Portfolio";
 import Toolbar from "./components/Toolbar";
-import {Await, Route, Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
 import Arena from "./pages/Arena";
 import SocialMediaApp from "./pages/SocialMediaApp";
+import {useEffect} from "react";
 
 function App() {
-  return (
-    <div>
-        <Toolbar/>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/portfolio/arena" element={<Arena />}/>
-            <Route path="/portfolio/social-media-app" element={<SocialMediaApp />}/>
-        </Routes>
-    </div>
-  );
+    useEffect(() => {
+        fetch("https://send-email-api-124q.onrender.com/wakeup")
+            .then(res => res.json())
+            .catch(err => {})
+    }, [])
+    return (
+        <div>
+            <Toolbar/>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/portfolio" element={<Portfolio/>}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="/contact" element={<Contact/>}/>
+                <Route path="/portfolio/arena" element={<Arena/>}/>
+                <Route path="/portfolio/social-media-app" element={<SocialMediaApp/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;

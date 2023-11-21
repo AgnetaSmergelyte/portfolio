f656eb67a297b9463efdcd63228ae60df7ec61c0
spegisima <agneta.smergelyte@gmail.com>
2023-11-21 15:28:17 +0200

    meta

--- src/App.js
+++ src/App.js
@@ -9,21 +9,10 @@ import Arena from "./pages/Arena";
 import SocialMediaApp from "./pages/SocialMediaApp";
 import React from "react";
 import GoUp from "./components/GoUp";
-import { Helmet } from 'react-helmet';
-import profilePic from './images/profile-pic.png';
 
 function App() {
     return (
         <div>
-            <Helmet>
-                <title>Agneta Šmergelytė</title>
-                <meta name="description" content="Web Developer Portfolio" />
-                <meta property="og:title" content="Agneta Šmergelytė" />
-                <meta property="og:description" content="Web Developer Portfolio" />
-                <meta property="og:image" content={profilePic} />
-                <meta property="og:url" content="https://www.agnetasmergelyte.lt/" />
-                <meta property="og:type" content="website" />
-            </Helmet>
             <Toolbar/>
             <Routes>
                 <Route path="/" element={<Home/>}/>
